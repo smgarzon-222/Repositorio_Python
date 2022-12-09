e11d1467c12a66831d953802984f5208ab5a205a
smgarzon-222 <smgarzon222@gmail.com>
2022-12-09 16:18:53 -0500

    commit

--- EstructuraControl4.py
+++ EstructuraControl4.py
@@ -1,7 +1,6 @@
 # Escriba un programa que pida un nombre de usuario y una contraseña
 # y si se ha introducido "pepe" y "asdasd" se indica "Has ingresado al sistema"
 # sino se da un error.
-
 usuario = input("Ingrese su usuario: ")
 contrasena = input("Ingrese su contraseña: ")
 if usuario == "jmorenog" and contrasena == "C0l0mb142022.":
